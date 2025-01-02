737b2ef6d2f0238247b6f0bf20b32be73fb0c433
galihpermana29 <galihcbn123@gmail.com>
2025-01-02 21:56:01 +0700

    bug

--- src/components/spotify/player.tsx
+++ src/components/spotify/player.tsx
@@ -15,7 +15,7 @@ export default function Player({
     <div className="fixed bottom-0 left-0 right-0 bg-black border-t border-[#282828] px-2 md:px-4 py-2 md:py-3 z-[9999]">
       <div className="flex items-center justify-between gap-2">
         <PlayerTrackInfo imageUri={imageUri} />
-        <PlayerControls />
+        <PlayerControls modalContent={modalContent} />
         <PlayerVolume />
       </div>
     </div>
