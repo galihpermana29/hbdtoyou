337298b0e0e0b86deff65b8069cd6540d1c10dcf
galihpermana29 <galihcbn123@gmail.com>
2025-10-10 00:25:17 +0700

    test

--- src/components/newlanding/NewLandingPage.tsx
+++ src/components/newlanding/NewLandingPage.tsx
@@ -56,7 +56,7 @@ export default function NewLandingPage() {
       return data.data;
     },
   });
-  console.log(process.env.NEXT_PUBLIC_APP_ENV, process.env.NEXT_PUBLIC_STAGING_API)
+  console.log(process.env.NEXT_PUBLIC_APP_ENV, process.env.NEXT_PUBLIC_STAGING_API, process.env.NEXT_PUBLIC_API_URI)
 
   return (
     <div>
